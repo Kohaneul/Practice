804b2fb0ab3b30783e6dda7a01df3bf641b5ed09
Kohaneul <96707563+Kohaneul@users.noreply.github.com>
2022-07-17 20:44:06 +0900

    0717 commit

--- deliveryproject/src/main/java/com/githrd/deli/controller/FaqController.java
+++ deliveryproject/src/main/java/com/githrd/deli/controller/FaqController.java
@@ -1,8 +1,9 @@
 package com.githrd.deli.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,80 +13,96 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.githrd.deli.service.AdminService;
 import com.githrd.deli.service.FaqService;
+import com.githrd.deli.vo.adminVO;
 import com.githrd.deli.vo.faqVO;
 
 @Controller
 @RequestMapping("/faq")
 public class FaqController {
 	
-	@Autowired private FaqService service;
+	@Autowired	private AdminService adminService;
+	@Autowired 	private FaqService faqService;
 	
-	private Map<Integer,faqVO> faqMap = new HashMap<>();	//no, faqvo
+	private static adminVO admin;
 	
-	
-	//faq 메인페이지 보기
+	// faq 메인페이지 보기
 	@GetMapping("/board.dlv")
-	public String faqMain(Model model) {
-		List<faqVO> list = service.selectList();
-		if(list!=null) {
+	public String boardView(HttpServletRequest request,Model model) {
+		String uri = request.getRequestURI();
+		List<faqVO> list = faqService.selectList();
+		model.addAttribute("list", list);
+		model.addAttribute("admin",admin);
+		return faqService.findViewPage(uri);
+	}
+	
+	
+	//로그인페이지
+	@GetMapping("/admin/board/login.dlv")
+	public String login() {
+		return	"/faq/admin/adminLogin";
+	}
+	
+	
+	@PostMapping("/admin/board/login.dlv")
+	public String login(Model model, @Param("id")String id, @Param("pw")String pw) {
+		String uri ="/deli/faq/board.dlv";
+		String msg = adminService.LoginMsg(id, pw);
+		if(msg==null) {
+			admin = adminService.selectId(id);
+			List<faqVO> list = faqService.selectList();
 			model.addAttribute("list", list);
-			System.out.println("list");
+			model.addAttribute("admin",admin);
 		}
 		else {
-			String str = "조회된 데이터가 없습니다.";
-			model.addAttribute("str",str);
-		}
-		return "/faq/faqList";
-	}
-	
-	//faq 페이지 상세내용 보기
-	@GetMapping("/board/detail.dlv")
-	public String faqDetail(Model model,@Param("no")int no) {
-		faqVO faq = service.selectNum(no);
-		model.addAttribute("faq",faq);
-		return "/faq/faqDetail";
-	}
-	
-	//faq 입력
-	@GetMapping("/board/insert.dlv")
-	public String faqInsert() {
-		return"/faq/faqInsert";
-	}
+			uri="/deli/faq/admin/board/login.dlv";
 
-	
-	//faq 입력
-	@PostMapping("/board/insert.dlv")
-	public String faqInsert(@Param("id")String id,@Param("pw")String pw,@Param("title")String title, @Param("content")String content){
-		//no,id,pw,title,content
-		faqVO vo = new faqVO();
-		vo.setId(id);
-		vo.setPw(pw);
-		vo.setTitle(title);
-		vo.setContent(content);
-		System.out.println(vo);
-		service.insert(faqMap, vo);
-		return "redirect:/faq/board.dlv";
-	}
-	//faq 페이지 수정페이지
-	@GetMapping("/board/update.dlv")
-	public String faqUpdate(Model model,@Param("no")int no) {
-		faqVO faq = faqMap.get(no);
-		model.addAttribute("no",no);
-		model.addAttribute("faq",faq);
-		return"/faq/faqUpdate";
-	}
-	//faq 페이지 
-	@PostMapping("/board/update.dlv")
-	public String faqUpdate2(Model model,@Param("no")int no,@Param("title")String title, @Param("content")String content) {
-		service.update(faqMap, no,title, content);
-		return "redirect:/faq/board.dlv";
+		}
+		return faqService.findViewPage(uri);
 	}
 	
-	@GetMapping("/board/delete.dlv")
-	public String faqDelete(@Param("no")int no) {
-		service.delete(faqMap,no);
-		return"redirect:/faq/board.dlv";
+	
+	
+	
+	
+	
+	@GetMapping("/admin/board/insert.dlv")
+	public String beforeInsert(HttpServletRequest request,Model model) {
+		String uri = request.getRequestURI();
+		model.addAttribute("admin", admin);
+		return faqService.findViewPage(uri);
+	}
+	
+	
+	
+	@PostMapping("/admin/board/insert.dlv")
+	public String afterInsert(Model model, @Param("id")String id, @Param("title")String title,@Param("content")String content) {
+		String uri ="/deli/faq/board.dlv";
+		faqVO faq = new faqVO(id,title,content);
+		faqService.insert(faq);
+		List<faqVO> list = faqService.selectList();
+		model.addAttribute("list", list);
+		return faqService.findViewPage(uri);
+	}
+	
+	
+	@RequestMapping(value = {"/board/detail.dlv", "/admin/board/delete.dlv","/admin/board/update.dlv","/admin/board/update2.dlv"})
+	public String detailView(HttpServletRequest request, @Param("no")int no,Model model) {
+		faqVO faq = faqService.selectNum(no);
+		String uri = request.getRequestURI();
+		model.addAttribute("admin",admin);
+		if(uri.equals("/deli/faq/admin/board/update2.dlv"))
+		{
+			faqService.update(no, request.getParameter("title"), request.getParameter("content"));
+			
+		}		
+		if(uri.equals("/deli/faq/admin/board/delete.dlv"))
+		{
+			faqService.delete(no);
+		}
+		model.addAttribute("faq", faq);
+		return faqService.findViewPage(uri);
 	}
 	
 }
